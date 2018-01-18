23dd8c28a3953e6deb0a463a6dcbe9217080926b
kwon <kkdosk@naver.com>
2018-01-18 10:46:24 +0900

    first

---
+++ tutorial/ch06/ch_06_1/src/main.rs
@@ -0,0 +1,41 @@
+#![allow(unused_variables)]
+fn main() {
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    
+    // let sum = x + y;
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    value_in_cents(Coin::Penny);
+}
