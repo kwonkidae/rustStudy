cc0a7aae721ffdde82b3189b81ca118d7040a08c
kwon <kkdosk@naver.com>
2018-04-25 10:21:57 +0900

    asdf

--- rust-gentle/ch07/src/main.rs
+++ rust-gentle/ch07/src/main.rs
@@ -3,6 +3,8 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use std::thread;
 use std::time;
+use std::sync::Arc;
+use std::sync::mpsc;
 
 fn main() {
 	let answer = Cell::new(42);
@@ -52,9 +54,91 @@ fn main() {
 			println!("hello {}", i);
 		});
 		threads.push(t);
+
 	}
 
 	for t in threads {
 		t.join().expect("thread failed");
+	}
+
+	let name = "dolly".to_string();
+	// move가 없으면 컴파일 에러
+	let t = thread::spawn(move || {
+		println!("hello {}", name);
+	});
+	println!("wait {:?}", t.join());
+
+	{
+		let name = "dolly";
+		let t1 = thread::spawn(move || {
+			println!("hello {}", name);
+		});
+
+		let t2 = thread::spawn(move || {
+			println!("goodbye{} ", name);
+		});
+
+		println!("{:?}", t1.join());
+		println!("{:?}", t2.join());
+	}
+
+	{
+		let mut threads = Vec::new();
+		let name = Arc::new(MyString::new("dolly"));
+
+		for i in 0..5 {
+			let tname = name.clone();
+			let t = thread::spawn(move || {
+				println!("hello {} count {}", tname.0, i);
+			});
+			threads.push(t);
+		}
+
+		for t in threads {
+			t.join().expect("thread failed");
+		}
+	}
+
+	{
+		let nthreads = 5;
+		let (tx, rx) = mpsc::channel();
+
+		for i in 0..nthreads {
+			let tx = tx.clone();
+			thread::spawn(move || {
+				let response = format!("hello {}", i);
+				tx.send(response).unwrap();
+			});
+		}
+
+		for _ in 0..nthreads {
+			println!("got {:?}", rx.recv());
+		}
+	}
+
+	{
+		// block send operation 
+		let (tx, rx) = mpsc::sync_channel(4);
+
+		let t1 = thread::spawn(move || {
+			for i in 0..5 {
+				tx.send(i).unwrap();
+			}
+		});
+
+		// for _ in 0..5 {
+		// 	let res = rx.recv().unwrap();
+		// 	println!("{}", res);
+		// }
+		t1.join().unwrap();
+	}
+
+}
+
+struct MyString(String);
+
+impl MyString {
+	fn new(s: &str) -> MyString {
+		MyString(s.to_owned())
 	}
 }
